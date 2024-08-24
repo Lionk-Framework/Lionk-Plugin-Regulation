14259d6543bd9d18abaa2137f1122b5725fde7f7
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-24 13:34:42 +0200

    update submodule and work on pump

--- src/Regulation.Components/Pump.cs
+++ src/Regulation.Components/Pump.cs
@@ -1,51 +1,93 @@
 ﻿// Copyright © 2024 Lionk Project
 
+using Lionk.Core;
 using Lionk.Core.Component;
 using Lionk.Rpi.Gpio;
+using Newtonsoft.Json;
 
 namespace Regulation.Components;
 
 /// <summary>
 /// This class represents a pump.
 /// </summary>
+[NamedElement("Pump", "Pump with inversed PWM control")]
 public class Pump : BaseExecutableComponent
 {
-    /// <summary>
-    /// Gets or sets the speed of the pump.
-    /// </summary>
-    public double Speed { get; set; }
+    #region Private Fields
 
     private Guid _pwmPinId;
-    private IComponentService _componentService;
-    private StandardPwmGpio? _pwmGpio;
+
+    #endregion Private Fields
+
+    #region Private Properties
+
+    private double DutyCycle => 1 - Speed;
+
+    #endregion Private Properties
+
+    #region Public Properties
+
+    /// <inheritdoc/>
+    public override bool CanExecute
+        => PwmGpio is not null
+        && PwmGpio.CanExecute;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Pump"/> class.
+    /// Gets or sets the PWM GPIO.
     /// </summary>
-    /// <param name="componentService"> The component service. </param>
-    public Pump(IComponentService componentService) => _componentService = componentService;
+    [JsonIgnore]
+    public StandardPwmGpio? PwmGpio { get; set; }
 
     /// <summary>
     /// Gets or sets the PWM pin id.
     /// </summary>
-    public Guid PwmPinId
+    public Guid PwmGpioId
     {
         get => _pwmPinId;
-        set
-        {
-            _pwmGpio = (StandardPwmGpio?)_componentService.GetInstanceByID(value);
-            SetField(ref _pwmPinId, value);
-        }
+        set => SetField(ref _pwmPinId, value);
     }
 
-    /// <inheritdoc/>
-    public override bool CanExecute => _pwmGpio != null;
+    /// <summary>
+    /// Gets or sets the speed of the pump.
+    /// </summary>
+    [JsonIgnore]
+    public double Speed { get; set; }
+
+    #endregion Public Properties
+
+    #region Protected Methods
 
     /// <inheritdoc/>
     protected override void OnExecute(CancellationToken cancellationToken)
     {
+        if (PwmGpio is null) return;
         base.OnExecute(cancellationToken);
-        if (_pwmGpio is null) return;
-        _pwmGpio.DutyCycle = Speed;
+        PwmGpio.DutyCycle = DutyCycle;
+        PwmGpio.Execute();
     }
+
+    #endregion Protected Methods
+
+    #region Public Methods
+
+    /// <inheritdoc/>
+    public override void Abort()
+    {
+        if (PwmGpio is null) return;
+        base.Abort();
+        Speed = 0;
+        PwmGpio.DutyCycle = DutyCycle;
+        PwmGpio.Execute();
+    }
+
+    /// <summary>
+    /// Initializes the component.
+    /// </summary>
+    public override void Dispose()
+    {
+        base.Dispose();
+        PwmGpio?.Dispose();
+    }
+
+    #endregion Public Methods
 }
